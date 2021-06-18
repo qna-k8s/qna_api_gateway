b7785bb2f7674ad9b6d061ce61bdb450d700401e
Trivedh Audurthi <audurthi.t@northeastern.edu>
2021-06-18 10:27:31 -0400

    added ci/cd using github actions

---
+++ deploy_api_gateway_chart.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+var=$(helm status qna-agr 2>&1)
+errorMsg="Error: release: not found"
+installCmd="helm install qna-agr ./helm_chart/qna_apigateway_chart \
+            --set imageName=$imageName \
+              DB_PASSWORD=$dbPassword"
+updateCmd="helm upgrade qna-agr \
+            --set imageName=$imageName \
+            DB_PASSWORD=$dbPassword"
+echo "$var"
+if [ "$var" = "$errorMsg" ]
+then
+    eval $installCmd
+else
+    eval $updateCmd
+fi
---
+++ deploy_mysql_chart.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+var=$(helm status qna-msr 2>&1)
+errorMsg="Error: release: not found"
+installCmd="helm install qna-msr ./helm_chart/qna_mysql \
+            --set DB_NAME=$dbName \
+              DB_PASSWORD=$dbPassword"
+updateCmd="helm upgrade qna-msr \
+            --set DB_NAME=$dbName \
+            DB_PASSWORD=$dbPassword"
+echo "$var"
+if [ "$var" = "$errorMsg" ]
+then
+    eval $installCmd
+else
+    eval $updateCmd
+fi
