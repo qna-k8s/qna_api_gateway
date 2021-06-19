a132058c72dfb7028084cd8e6082cca25c321dfb
Trivedh Audurthi <audurthi.t@northeastern.edu>
2021-06-18 22:16:22 -0400

    fixed commands

--- deploy_api_gateway_chart.sh
+++ deploy_api_gateway_chart.sh
@@ -3,7 +3,7 @@ var=$(helm status qna-agr 2>&1)
 errorMsg="Error: release: not found"
 installCmd="helm install qna-agr ./helm_chart/qna_apigateway_chart \
             --set imageName=$imageName,DB_PASSWORD=$dbPassword"
-updateCmd="helm upgrade qna-agr \
+updateCmd="helm upgrade qna-agr ./helm_chart/qna_apigateway_chart \
             --set imageName=$imageName,DB_PASSWORD=$dbPassword"
 echo "$var"
 if [ "$var" = "$errorMsg" ]
--- deploy_mysql_chart.sh
+++ deploy_mysql_chart.sh
@@ -3,7 +3,7 @@ var=$(helm status qna-msr 2>&1)
 errorMsg="Error: release: not found"
 installCmd="helm install qna-msr ./helm_chart/qna_mysql \
             --set DB_NAME=$dbName,DB_PASSWORD=$dbPassword"
-updateCmd="helm upgrade qna-msr \
+updateCmd="helm upgrade qna-msr ./helm_chart/qna_mysql \
             --set DB_NAME=$dbName,DB_PASSWORD=$dbPassword"
 echo "$var"
 if [ "$var" = "$errorMsg" ]
