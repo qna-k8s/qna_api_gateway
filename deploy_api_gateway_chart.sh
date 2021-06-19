4a4ae733187b8d66f7c558162a197096dcb7ed92
Trivedh Audurthi <audurthi.t@northeastern.edu>
2021-06-18 21:38:50 -0400

    fixed the command

--- deploy_api_gateway_chart.sh
+++ deploy_api_gateway_chart.sh
@@ -2,11 +2,9 @@
 var=$(helm status qna-agr 2>&1)
 errorMsg="Error: release: not found"
 installCmd="helm install qna-agr ./helm_chart/qna_apigateway_chart \
-            --set imageName=$imageName \
-              DB_PASSWORD=$dbPassword"
+            --set imageName=$imageName,DB_PASSWORD=$dbPassword"
 updateCmd="helm upgrade qna-agr \
-            --set imageName=$imageName \
-            DB_PASSWORD=$dbPassword"
+            --set imageName=$imageName,DB_PASSWORD=$dbPassword"
 echo "$var"
 if [ "$var" = "$errorMsg" ]
 then
--- deploy_mysql_chart.sh
+++ deploy_mysql_chart.sh
@@ -2,11 +2,9 @@
 var=$(helm status qna-msr 2>&1)
 errorMsg="Error: release: not found"
 installCmd="helm install qna-msr ./helm_chart/qna_mysql \
-            --set DB_NAME=$dbName \
-              DB_PASSWORD=$dbPassword"
+            --set DB_NAME=$dbName,DB_PASSWORD=$dbPassword"
 updateCmd="helm upgrade qna-msr \
-            --set DB_NAME=$dbName \
-            DB_PASSWORD=$dbPassword"
+            --set DB_NAME=$dbName,DB_PASSWORD=$dbPassword"
 echo "$var"
 if [ "$var" = "$errorMsg" ]
 then
